9ce52a484726fa102b27c927282d131741265ec6
Sasanga Edirisinghe <madhumaledirisinghe@gmail.com>
2024-04-24 00:32:47 +0530

    created new .net 8 exception handler

---
+++ ExceptionDemo/Middleware/ExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using ExceptionDemo.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ExceptionDemo.Middleware
+{
+    public class ExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            var statusCode = exception switch
+            {
+                NotFoundException => (int)HttpStatusCode.NotFound,
+                UnauthorizedAccessException => (int)HttpStatusCode.Unauthorized,
+                ValidationException => (int)HttpStatusCode.Unauthorized,
+                _ => (int)HttpStatusCode.InternalServerError
+            };
+
+            await httpContext.Response.WriteAsJsonAsync(new ProblemDetails
+            {
+                Status = statusCode,
+                Type = exception.GetType().Name,
+                Title = exception.Message,
+                Detail = exception.StackTrace,
+                Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}"
+            });
+
+            return true;
+        }
+    }
+}
